86529a18ec52b121affafa2bd4ae07f67ab2bf58
Gregory Moore <grmoore18@gmail.com>
2017-09-16 21:23:10 -0700

    Add three-Dragcontrols definitions

---
+++ types/three-dragcontrols/index.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for three-dragcontrols 0.86
+// Project: https://github.com/keqingrong/three-dragcontrols#readme
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+import {Camera} from 'three';
+
+export default class DragControls {
+    constructor(objects: any[], camera: Camera, domElement: HTMLElement);
+}
